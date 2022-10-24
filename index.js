0a9f2b3e1f25c50c7588adb17559fc9479975ee8
serajussaleheenstudy <serajussaleheen74@gmail.com>
2022-10-24 06:52:06 +0600

    done

--- index.js
+++ index.js
@@ -6,18 +6,36 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 
 const categories = require('./data/categories.json');
-const news = require('.data/news.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
-    res.send('News API running')
+    res.send('News API running');
 });
 
 app.get('/news-categories', (req, res) => {
     res.send(categories);
 });
 
-app.get('/news/:id', (req, res) => {
-    console.log(req.params)
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (id === '08') {
+        res.send(news);
+    }
+    else {
+        const category_news = news.filter(n => n.category_id === id);
+        res.send(category_news);
+    }
 });
 
-app.listen(port, () => console.log('news server running'))
+app.get('/news', (re, res) => {
+    res.send(news);
+});
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find(n => n._id === id);
+    res.send(selectedNews);
+});
+
+app.listen(port, () => console.log('news server running'));
